4c27dd814cec0e70f8d39690f61078f39e4a8cbb
Foenki <pierrot.lagier@wanadoo.fr>
2018-09-29 23:03:21 +0200

    Clean-up 2D visualization

--- src/Visualization2D/TorusPanel.java
+++ src/Visualization2D/TorusPanel.java
@@ -2,13 +2,17 @@ package Visualization2D;
 
 import TorusPuzzle.TorusPuzzle;
 
-import java.awt.*;
-
+import java.awt.Graphics;
+import java.awt.Font;
 import javax.swing.JPanel;
 
 public class TorusPanel extends JPanel
 {
     private TorusPuzzle puzzle;
+    private static final Font VALUES_FONT = new Font("Courier", Font.BOLD, 20);
+    private static final int VALUE_SHIFT_X = 20;
+    private static final int VALUE_SHIFT_Y = 35;
+    private static final int DIGIT_SHIFT_X = 6;
 
     public TorusPanel(TorusPuzzle puzzle)
     {
@@ -18,28 +22,37 @@ public class TorusPanel extends JPanel
 
     public void paintComponent(Graphics g)
     {
-        Font font = new Font("Courier", Font.BOLD, 20);
-        g.setFont(font);
+        drawValues(g);
+        drawLines(g);
+    }
+
+    public void drawValues(Graphics g)
+    {
+        g.setFont(VALUES_FONT);
         for(int i = 0; i < puzzle.rowDim(); ++i)
         {
-            int lineY = i * 50;
-            g.drawLine(0, lineY, puzzle.colDim() * 50, lineY);
+            int lineY = i * Window2D.CASE_DIMENSION;
             for(int j = 0; j < puzzle.colDim(); ++j)
             {
-                int columnX = j * 50;
-                if(i == 0)
-                {
-                    g.drawLine(columnX, 0, columnX, puzzle.rowDim()*50);
-                }
+                int columnX = j * Window2D.CASE_DIMENSION;
                 int value = puzzle.getValue(i, j);
-                int nbDigits = 1+(int)Math.log10(value);
-                g.drawString("" + value, 20 + columnX - (nbDigits-1) * 6, 35 + lineY);
+                int nbDigits = 1 + (int)Math.log10(value);
+                g.drawString("" + value, columnX + VALUE_SHIFT_X - (nbDigits-1) * DIGIT_SHIFT_X, lineY + VALUE_SHIFT_Y);
             }
         }
-        g.drawLine(0, 50 * puzzle.rowDim(), puzzle.colDim() * 50, 50 * puzzle.rowDim());
-        g.drawLine(50 * puzzle.colDim(), 0, puzzle.colDim() * 50, 50 * puzzle.rowDim());
+    }
 
+    public void drawLines(Graphics g)
+    {
+        for(int i = 0; i <= puzzle.rowDim(); ++i)
+        {
+            g.drawLine(0, i * Window2D.CASE_DIMENSION, puzzle.colDim() * Window2D.CASE_DIMENSION, i * Window2D.CASE_DIMENSION);
+        }
 
+        for(int j = 0; j <= puzzle.colDim(); ++j)
+        {
+            g.drawLine(j * Window2D.CASE_DIMENSION, 0, j * Window2D.CASE_DIMENSION, puzzle.rowDim() * Window2D.CASE_DIMENSION);
+        }
     }
 
 }
--- src/Visualization2D/Window2D.java
+++ src/Visualization2D/Window2D.java
@@ -3,33 +3,39 @@ package Visualization2D;
 import TorusPuzzle.TorusPuzzle;
 import TorusPuzzle.MoveSequence;
 
-import javax.swing.*;
-import javax.swing.border.Border;
-import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
 
 public class Window2D extends JFrame implements ActionListener
 {
-    private final int CASE_DIMENSION = 50;
+    public static final int CASE_DIMENSION = 50;
+    private static final int SHIFT_X_BORDER = 7;
+    private static final int SHIFT_Y_BORDER = 36;
+    private static final int SHIFT_Y_BUTTONS = 45;
     private TorusPuzzle puzzle;
-    JButton shuffleButton = new JButton("Shuffle");
-    JButton solveButton = new JButton("Solve");
-    MoveSequence sequence = new MoveSequence();
+    private TorusPanel torusPanel;
+    private JButton shuffleButton = new JButton("Shuffle");
+    private JButton solveButton = new JButton("Solve");
+    private MoveSequence sequence = new MoveSequence();
 
     public Window2D(TorusPuzzle puzzle)
     {
         this.puzzle = puzzle;
         this.setTitle("TorusPuzzle");
         this.setResizable(false);
-        this.setSize(puzzle.colDim() * CASE_DIMENSION + 7, puzzle.rowDim() * CASE_DIMENSION + 36 + 45);
+        this.setSize(puzzle.colDim() * CASE_DIMENSION + SHIFT_X_BORDER, puzzle.rowDim() * CASE_DIMENSION + SHIFT_Y_BORDER + SHIFT_Y_BUTTONS);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new TorusPanel(puzzle);
 
         JPanel mainContainer = new JPanel();
         mainContainer.setLayout(new BorderLayout());
-        mainContainer.add(panel, BorderLayout.CENTER);
+
+        torusPanel = new TorusPanel(puzzle);
+        mainContainer.add(torusPanel, BorderLayout.CENTER);
 
         JPanel buttonsContainer = new JPanel();
         shuffleButton.addActionListener(this);
@@ -40,7 +46,6 @@ public class Window2D extends JFrame implements ActionListener
 
         this.setContentPane(mainContainer);
         this.setVisible(true);
-
     }
 
     public void actionPerformed(ActionEvent event)
@@ -51,9 +56,7 @@ public class Window2D extends JFrame implements ActionListener
             puzzle.shuffle(100, shuffleSeq);
             sequence.add(shuffleSeq);
         }
-
-
-        if(event.getSource() == solveButton)
+        else if(event.getSource() == solveButton)
         {
             puzzle.execute(sequence.reverseSequence());
             sequence = new MoveSequence();
@@ -61,5 +64,4 @@ public class Window2D extends JFrame implements ActionListener
 
         repaint();
     }
-
 }
